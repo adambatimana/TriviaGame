9b8226e7ed73c2c4b19595d5297fe6de1fb02bb1
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-05-07 21:09:58 -0400

    pseudo coded

---
+++ assets/js/app.js
@@ -0,0 +1,87 @@
+window.onload = function(){
+//Variables to be used in the game
+
+
+// =============================================================================
+//                          FUNCTIONS
+// =============================================================================
+
+//gameStart()
+
+//hide gameStage
+
+
+
+
+// restart game()
+
+// restart timer
+// clear score
+// clear timer
+// clear selections
+
+
+
+
+//checkWin()
+
+
+// time runs out()
+
+
+//TIMER()
+
+// ***************** IF TIME SWAP OUT QUETSION PANELS *******************
+
+
+
+// =============================================================================
+//                             LOGIC
+// =============================================================================
+
+
+///hide gameStage and show click to begin
+ $("#begin").on("click", function () {
+   $(".gameStage").show();
+ });
+// on click event listener
+$(document).on("click", "radio", function () {
+
+    //if, for question one, the correct answer is selected add point to correct answer counter
+
+
+
+    //if, for question one, the incorrect answer is selected add point to wrong answer counter
+
+
+
+    //else, for question one, none are selected then  add point to not answered array
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
